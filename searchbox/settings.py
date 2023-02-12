800af9b9a881de23ceb6ddacf8d3a1118624e30a
Diego Veralli <diegoov@gmail.com>
2023-02-12 20:44:46 +0100

    Increase concurrent requests to 12

--- searchbox/settings.py
+++ searchbox/settings.py
@@ -22,7 +22,7 @@ USER_AGENT = 'searchbox (+http://diegoveralli.net)'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 8
+CONCURRENT_REQUESTS = 12
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
